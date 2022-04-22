e5f1184d7a0083b787e03af1e1cb56cde8abc648
Matheus Kruger <ms-kruger@hotmail.com>
2022-04-22 15:41:10 -0300

    using the server as a npm package, validating if discord is running

--- app.js
+++ app.js
@@ -5,45 +5,37 @@ const {
 const { exec } = require("child_process");
 const url = require("url");
 const path = require("path");
+const { api } = require("nintendo-discord-rich-presence-server/src/api");
 
 let appWindow;
 let serverProcess;
 
-function killServer() {
-    if (serverProcess.killed) return;
-
-    exec("kill " + (Number.parseInt(serverProcess.pid) + 1), (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
+const isRunning = (query, cb) => {
+    let platform = process.platform;
+    let cmd = '';
+    switch (platform) {
+        case 'win32' : cmd = `tasklist`; break;
+        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
+        case 'linux' : cmd = `ps -A`; break;
+        default: break;
+    }
+    exec(cmd, (err, stdout, stderr) => {
+        stdout = stdout.replaceAll(query + " <defunct>", "");
+        cb(stdout.includes(query));
     });
+}
+
+function killServer() {
+    if (serverProcess) {
+        serverProcess.close();
+    }
 }
 
 function startServer() {
-    serverProcess = exec("node server/src/index.js &", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-
-        console.log("Server iniciado", stdout);
-    });
+    serverProcess = api.listen(3333);
 }
 
 function initWindow() {
-    startServer();
-    
     appWindow = new BrowserWindow({
         width: 1000,
         height: 800,
@@ -52,15 +44,26 @@ function initWindow() {
         }
     });
 
-    appWindow.setMenuBarVisibility(false);
+    isRunning('Discord', (status) => {
+        let fileToRender = "/dist/index.html";
+        
+        if (status) {
+            startServer();
+        }
+        else {
+            fileToRender = "/discord-error.html";
+        }
 
-    appWindow.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/dist/index.html`),
-            protocol: "file:",
-            slashes: true
-        })
-    );
+        appWindow.loadURL(
+            url.format({
+                pathname: path.join(__dirname, fileToRender),
+                protocol: "file:",
+                slashes: true
+            })
+        );
+    });
+
+    appWindow.setMenuBarVisibility(false);
 
     appWindow.on('closed', function () {
         appWindow = null;
