e13aac1791929ebc3564201bcae089c40cc202d5
Matheus Kruger <ms-kruger@hotmail.com>
2022-04-22 10:43:32 -0300

    making the electron bundle lighter, refactoring scripts

---
+++ electron_scripts/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm resources/app/karma.conf.js
+rm resources/app/angular.json
+rm resources/app/README.md
+rm resources/app/tsconfig.app.json
+rm resources/app/tsconfig.json
+rm resources/app/tsconfig.spec.json
+rm resources/app/.gitignore
+rm resources/app/.editorconfig
+rm resources/app/.browserslistrc
+rm -r resources/app/electron_scripts/
+rm -r resources/app/node_modules/
+rm -r resources/app/src/
